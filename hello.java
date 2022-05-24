54c595230b8938816eefceb0a4228ad6e9b66a79
bobby12312 <104480010+bobby12312@users.noreply.github.com>
2022-05-23 20:59:40 -0600

    java 1

--- hello.java
+++ hello.java
@@ -1,8 +1,32 @@
-class Hello
-    public static void main (String args[]) {
-        System.out.println("Hello World");
+public class day3Uc5{
+    public static void main (string[]args){
+        final int PART_TIME = 1;
+        final int FULL_TIME = 2;
+        final int WAGE_PER_HR = 20;
+        final int WOKING_DAYS = 20;
         
-        System.out.println("Hello");
-    
-}
-}
+        int totalwage = 0;
+        for (int day = 1; day <= WORKING_DAYS; day++)
+        {
+            int empType = (int) (MATH.random() * 100) % 3;
+            int workinghours =0;
+            switch (empType)
+            {
+                case FULL_TIME:
+                    workinghours = 8;
+                    break;
+                case PART_TIME:
+                    workinghours = 4;
+                    break;
+                default:
+            }
+            int wage = workinghours * WAGE_PER_HR;
+            system.out.println("Day" + day +wage is:" + wage);
+                               totalwage +=wage;
+                               }
+                               system.out.println("total wage for a month is " + totalwage);
+                               }
+                               }
+
+                              
+                        
